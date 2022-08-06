bacb3bbdb71685b326f553016f1de37700a1a240
Grigori L <lupasco.grigori@gmail.com>
2022-08-06 12:36:58 +0300

    welcome screen with login and password

--- app/src/main/java/com/example/coffeeorder/LoginActivity.kt
+++ app/src/main/java/com/example/coffeeorder/LoginActivity.kt
@@ -1,11 +1,41 @@
 package com.example.coffeeorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etName: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnMakeOrder: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        etName = findViewById(R.id.etPersonName)
+        etPassword = findViewById(R.id.etPassword)
+        btnMakeOrder = findViewById(R.id.btnMakeOrder)
+
+
+    }
+
+    fun onClickCreateOrder(view: View) {
+        val name = etName.text.toString().trim()
+        val password = etPassword.text.toString().trim()
+
+        if (name.isNotEmpty() && password.isNotEmpty()) {
+            intent = Intent(this, CreateOrderActivity::class.java)
+            intent.putExtra("name", name)
+            intent.putExtra("password", password)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, R.string.warning_fill_fields, Toast.LENGTH_SHORT).show()
+        }
     }
 }
